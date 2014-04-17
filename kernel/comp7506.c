8357fb29f71a1f879e6aa349269f2bd351636a3a
Derick Beckwith <derick.beckwith@gmail.com>
2014-04-17 11:57:40 -0400

    Fix missing comma

--- kernel/comp7506.c
+++ kernel/comp7506.c
@@ -1,7 +1,7 @@
 #include <linux/syscalls.h>
 #include <linux/printk.h>
 
-SYSCALL_DEFINE1(comp7506, int pid)
+SYSCALL_DEFINE1(comp7506, int, pid)
 {
 	printk("Processs %i made the Spring 2014, Auburn University COMP7500 system call.\n", pid);
 	return 0;
