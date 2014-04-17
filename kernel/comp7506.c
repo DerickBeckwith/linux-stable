08727605a751efc859dd19c1ecdcacbfc12d0af7
Derick Beckwith <derick.beckwith@gmail.com>
2014-04-17 18:14:48 -0400

    Add timestamp to kernel log msg

--- kernel/comp7506.c
+++ kernel/comp7506.c
@@ -3,8 +3,25 @@
 
 SYSCALL_DEFINE0(comp7506)
 {
+	/* Get the currently running process's process id. */
 	pid_t current_pid = current->pid;
-	printk("Processs %i made the Spring 2014, Auburn University COMP7506 system call.\n",
-			current_pid);
+
+	/* Get the current time. */
+	struct timeval t;
+	struct tm time_stamp;
+	int timezone_offset = sys_tz.tz_minuteswest * -60;
+	do_gettimeofday(&t);
+	time_to_tm(t.tv_sec, timezone_offset, &time_stamp);
+
+	/* Print to the kernel log. */
+	printk("Processs %d made the Spring 2014, "
+			"Auburn University COMP7506 system call at "
+			"%d:%d:%d:%ld\n",
+			current_pid,
+			time_stamp.tm_hour,
+			time_stamp.tm_min,
+			time_stamp.tm_sec,
+			t.tv_usec);
+
 	return 0;
 }
