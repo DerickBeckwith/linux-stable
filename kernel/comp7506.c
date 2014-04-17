0c328244add7603ceb7bdd983161aa444631b98e
Derick Beckwith <derick.beckwith@gmail.com>
2014-04-17 16:01:45 -0400

    Get current pid inside syscall

--- kernel/comp7506.c
+++ kernel/comp7506.c
@@ -1,8 +1,10 @@
 #include <linux/syscalls.h>
 #include <linux/printk.h>
 
-SYSCALL_DEFINE1(comp7506, int, pid)
+SYSCALL_DEFINE0(comp7506)
 {
-	printk("Processs %i made the Spring 2014, Auburn University COMP7500 system call.\n", pid);
+	pid_t current_pid = current->pid;
+	printk("Processs %i made the Spring 2014, Auburn University COMP7506 system call.\n",
+			current_pid);
 	return 0;
 }
